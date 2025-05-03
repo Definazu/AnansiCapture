d25e57fcee5c22d9b4ab1d50d37a5d2f84c20082
Definazu <inazumag@yandex.by>
2025-05-03 12:22:00 +0300

    README + Cosmetic fixes for CLI interface

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# AnansiCapture
-A simple cross-platform data traffic interception and analysis (NTA) tool written in Rust. 
+<h1 align="center">AnansiCapture</h1>
+<p align="center">
+<img alt="Static Badge" src="https://img.shields.io/badge/made_by-Definazu-red?style=for-the-badge&link=https%3A%2F%2Fgithub.com%2FDefinazu">
+<img alt="GitHub License" src="https://img.shields.io/github/license/Definazu/AnansiCapture?style=for-the-badge">
+<img alt="GitHub top language" src="https://img.shields.io/github/languages/top/Definazu/AnansiCapture?style=for-the-badge">
+<img alt="GitHub repo size" src="https://img.shields.io/github/repo-size/Definazu/AnansiCapture?style=for-the-badge">
+</p>
+
+## Definition
+A simple cross-platform data traffic interception and analysis (NTA) tool written in Rust.
+
+## Installation
+### For Linux
+> Firts of all you need install `npm`, `rustc`, `cargo`, `libpcap`.
+1. **Copy** the repository and go to the root directory:
+
+` git clone https://github.com/Definazu/AnansiCapture.git`
+
+2. **Building** a project:
+
+`cargo build`
+
+Well done!
+
+## Run
+### CLI
+`./target/debug/anansi -help`
+
+### GUI
+`cd gui`
+
+`npm install`
+
+`npm run tauri dev`
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -13,6 +13,8 @@ use pnet::packet::udp::UdpPacket;
 use pnet::packet::icmp::IcmpPacket;
 use pnet::packet::icmpv6::Icmpv6Packet;
 use pnet::packet::Packet;
+use colored::*;
+use chrono::Local;
 
 #[derive(Debug, Clone, ValueEnum)]
 enum PortRange {
@@ -43,13 +45,21 @@ struct Cli {
     /// Включить promiscuous режим
     #[arg(short = 'P', long)]
     promiscuous: bool,
+
+    /// Включить отладочный режим
+    #[arg(long)]
+    debug: bool,
 }
 
-struct PacketLogger;
+struct PacketLogger {
+    debug: bool,
+}
 
 impl anansi_core::capture::Observer for PacketLogger {
     fn update(&self, packet: &pcap::Packet) {
-        debug!("Получен пакет размером {} байт", packet.len());
+        if self.debug {
+            debug!("Получен пакет размером {} байт", packet.len());
+        }
         
         // Проверяем минимальный размер для Ethernet пакета
         if packet.len() < 14 {
@@ -59,95 +69,149 @@ impl anansi_core::capture::Observer for PacketLogger {
 
         match EthernetPacket::new(packet.data) {
             Some(ethernet) => {
-                debug!("Ethernet тип: {:?}", ethernet.get_ethertype());
+                if self.debug {
+                    debug!("Ethernet тип: {:?}", ethernet.get_ethertype());
+                }
                 
                 match ethernet.get_ethertype() {
                     EtherTypes::Ipv4 => {
                         if let Some(ipv4) = Ipv4Packet::new(ethernet.payload()) {
-                            debug!("IPv4 протокол: {:?}", ipv4.get_next_level_protocol());
+                            if self.debug {
+                                debug!("IPv4 протокол: {:?}", ipv4.get_next_level_protocol());
+                            }
                             
                             match ipv4.get_next_level_protocol() {
                                 IpNextHeaderProtocols::Tcp => {
                                     if let Some(tcp) = TcpPacket::new(ipv4.payload()) {
-                                        info!("TCP: {}:{} -> {}:{}", 
-                                            ipv4.get_source(), tcp.get_source(),
-                                            ipv4.get_destination(), tcp.get_destination());
-                                    } else {
+                                        let timestamp = Local::now().format("%H:%M:%S%.3f").to_string();
+                                        let flags = format_tcp_flags(&tcp);
+                                        println!("{} {} {}:{} > {}:{} {} {}",
+                                            timestamp.blue(),
+                                            "TCP".green(),
+                                            ipv4.get_source().to_string().yellow(),
+                                            tcp.get_source().to_string().cyan(),
+                                            ipv4.get_destination().to_string().yellow(),
+                                            tcp.get_destination().to_string().cyan(),
+                                            flags,
+                                            format!("{} bytes", packet.len()).magenta()
+                                        );
+                                    } else if self.debug {
                                         warn!("Не удалось разобрать TCP пакет");
                                     }
                                 },
                                 IpNextHeaderProtocols::Udp => {
                                     if let Some(udp) = UdpPacket::new(ipv4.payload()) {
-                                        info!("UDP: {}:{} -> {}:{}", 
-                                            ipv4.get_source(), udp.get_source(),
-                                            ipv4.get_destination(), udp.get_destination());
-                                    } else {
+                                        let timestamp = Local::now().format("%H:%M:%S%.3f").to_string();
+                                        println!("{} {} {}:{} > {}:{} {}",
+                                            timestamp.blue(),
+                                            "UDP".green(),
+                                            ipv4.get_source().to_string().yellow(),
+                                            udp.get_source().to_string().cyan(),
+                                            ipv4.get_destination().to_string().yellow(),
+                                            udp.get_destination().to_string().cyan(),
+                                            format!("{} bytes", packet.len()).magenta()
+                                        );
+                                    } else if self.debug {
                                         warn!("Не удалось разобрать UDP пакет");
                                     }
                                 },
                                 IpNextHeaderProtocols::Icmp => {
                                     if let Some(icmp) = IcmpPacket::new(ipv4.payload()) {
-                                        info!("ICMP: {} -> {}", 
-                                            ipv4.get_source(),
-                                            ipv4.get_destination());
-                                    } else {
+                                        let timestamp = Local::now().format("%H:%M:%S%.3f").to_string();
+                                        println!("{} {} {} > {} {}",
+                                            timestamp.blue(),
+                                            "ICMP".green(),
+                                            ipv4.get_source().to_string().yellow(),
+                                            ipv4.get_destination().to_string().yellow(),
+                                            format!("{} bytes", packet.len()).magenta()
+                                        );
+                                    } else if self.debug {
                                         warn!("Не удалось разобрать ICMP пакет");
                                     }
                                 },
-                                _ => debug!("IPv4: {} -> {}", 
-                                    ipv4.get_source(),
-                                    ipv4.get_destination()),
+                                _ => if self.debug {
+                                    debug!("IPv4: {} -> {}", 
+                                        ipv4.get_source(),
+                                        ipv4.get_destination());
+                                },
                             }
-                        } else {
+                        } else if self.debug {
                             warn!("Не удалось разобрать IPv4 пакет");
                         }
                     },
                     EtherTypes::Ipv6 => {
                         if let Some(ipv6) = Ipv6Packet::new(ethernet.payload()) {
-                            debug!("IPv6 протокол: {:?}", ipv6.get_next_header());
+                            if self.debug {
+                                debug!("IPv6 протокол: {:?}", ipv6.get_next_header());
+                            }
                             
                             match ipv6.get_next_header() {
                                 IpNextHeaderProtocols::Tcp => {
                                     if let Some(tcp) = TcpPacket::new(ipv6.payload()) {
-                                        info!("TCP: [{}]:{} -> [{}]:{}", 
-                                            ipv6.get_source(), tcp.get_source(),
-                                            ipv6.get_destination(), tcp.get_destination());
-                                    } else {
+                                        let timestamp = Local::now().format("%H:%M:%S%.3f").to_string();
+                                        let flags = format_tcp_flags(&tcp);
+                                        println!("{} {} [{}]:{} > [{}]:{} {} {}",
+                                            timestamp.blue(),
+                                            "TCP".green(),
+                                            ipv6.get_source().to_string().yellow(),
+                                            tcp.get_source().to_string().cyan(),
+                                            ipv6.get_destination().to_string().yellow(),
+                                            tcp.get_destination().to_string().cyan(),
+                                            flags,
+                                            format!("{} bytes", packet.len()).magenta()
+                                        );
+                                    } else if self.debug {
                                         warn!("Не удалось разобрать TCP пакет");
                                     }
                                 },
                                 IpNextHeaderProtocols::Udp => {
                                     if let Some(udp) = UdpPacket::new(ipv6.payload()) {
-                                        info!("UDP: [{}]:{} -> [{}]:{}", 
-                                            ipv6.get_source(), udp.get_source(),
-                                            ipv6.get_destination(), udp.get_destination());
-                                    } else {
+                                        let timestamp = Local::now().format("%H:%M:%S%.3f").to_string();
+                                        println!("{} {} [{}]:{} > [{}]:{} {}",
+                                            timestamp.blue(),
+                                            "UDP".green(),
+                                            ipv6.get_source().to_string().yellow(),
+                                            udp.get_source().to_string().cyan(),
+                                            ipv6.get_destination().to_string().yellow(),
+                                            udp.get_destination().to_string().cyan(),
+                                            format!("{} bytes", packet.len()).magenta()
+                                        );
+                                    } else if self.debug {
                                         warn!("Не удалось разобрать UDP пакет");
                                     }
                                 },
                                 IpNextHeaderProtocols::Icmpv6 => {
                                     if let Some(icmpv6) = Icmpv6Packet::new(ipv6.payload()) {
-                                        info!("ICMPv6: [{}] -> [{}]", 
-                                            ipv6.get_source(),
-                                            ipv6.get_destination());
-                                    } else {
+                                        let timestamp = Local::now().format("%H:%M:%S%.3f").to_string();
+                                        println!("{} {} [{}] > [{}] {}",
+                                            timestamp.blue(),
+                                            "ICMPv6".green(),
+                                            ipv6.get_source().to_string().yellow(),
+                                            ipv6.get_destination().to_string().yellow(),
+                                            format!("{} bytes", packet.len()).magenta()
+                                        );
+                                    } else if self.debug {
                                         warn!("Не удалось разобрать ICMPv6 пакет");
                                     }
                                 },
-                                _ => debug!("IPv6: [{}] -> [{}]", 
-                                    ipv6.get_source(),
-                                    ipv6.get_destination()),
+                                _ => if self.debug {
+                                    debug!("IPv6: [{}] -> [{}]", 
+                                        ipv6.get_source(),
+                                        ipv6.get_destination());
+                                },
                             }
-                        } else {
+                        } else if self.debug {
                             warn!("Не удалось разобрать IPv6 пакет");
                         }
                     },
-                    _ => debug!("Ethernet: {} -> {}", 
-                        ethernet.get_source(),
-                        ethernet.get_destination()),
+                    _ => if self.debug {
+                        debug!("Ethernet: {} -> {}", 
+                            ethernet.get_source(),
+                            ethernet.get_destination());
+                    },
                 }
             },
-            None => {
+            None => if self.debug {
                 warn!("Не удалось разобрать Ethernet пакет");
                 debug!("Сырые данные: {:?}", &packet.data[..std::cmp::min(32, packet.len())]);
             }
@@ -155,12 +219,28 @@ impl anansi_core::capture::Observer for PacketLogger {
     }
 }
 
+fn format_tcp_flags(tcp: &TcpPacket) -> String {
+    let mut flags = String::new();
+    let flags_byte = tcp.get_flags();
+    if flags_byte & 0x02 != 0 { flags.push('S'); } // SYN
+    if flags_byte & 0x10 != 0 { flags.push('A'); } // ACK
+    if flags_byte & 0x01 != 0 { flags.push('F'); } // FIN
+    if flags_byte & 0x04 != 0 { flags.push('R'); } // RST
+    if flags_byte & 0x08 != 0 { flags.push('P'); } // PSH
+    if flags_byte & 0x20 != 0 { flags.push('U'); } // URG
+    if flags_byte & 0x40 != 0 { flags.push('E'); } // ECE
+    if flags_byte & 0x80 != 0 { flags.push('C'); } // CWR
+    flags.red().to_string()
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug"))
-        .init();
-    
     let cli = Cli::parse();
+
+    // Настраиваем логирование
+    let log_level = if cli.debug { "debug" } else { "info" };
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or(log_level))
+        .init();
 
     if cli.list_interfaces {
         list_interfaces()?;
@@ -168,7 +248,7 @@ async fn main() -> Result<()> {
     }
 
     let facade = AnansiFacade::new();
-    let observer = Arc::new(PacketLogger);
+    let observer = Arc::new(PacketLogger { debug: cli.debug });
     let _observer_id = facade.add_observer(observer).await;
 
     let filter = create_filter(&cli)?;
@@ -190,11 +270,13 @@ async fn main() -> Result<()> {
 
 fn list_interfaces() -> Result<()> {
     let interfaces = pcap::Device::list()?;
-    println!("Доступные интерфейсы:");
+    println!("{}", "Доступные интерфейсы:".bold().underline());
     for interface in interfaces {
-        println!("{}", interface.name);
         if let Some(desc) = interface.desc {
-            println!("  Описание: {}", desc);
+            println!("{} -> {}",interface.name.bold().green(),desc);
+        }
+        else {
+            println!("{}", interface.name.bold().green());
         }
     }
     Ok(())
@@ -221,7 +303,6 @@ fn create_filter(cli: &Cli) -> Result<Option<String>> {
         } else if ports.contains(',') {
             // Список портов
             let ports_list: Vec<&str> = ports.split(',').collect();
-            let ports_filter = ports_list.join(" or port ");
             filter.push_str(&format!("(port {} or port {})", ports_list[0], ports_list[1..].join(" or port ")));
         } else {
             // Одиночный порт
