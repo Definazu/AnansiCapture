1335560934e01b9b1b3aa5012045059ecf62b27a
Definazu <inazumag@yandex.by>
2025-05-09 18:09:11 +0300

    Begin GUI design

---
+++ gui/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import '../app.css';
+</script>
+
+<div class="welcome-text">
+  Welcome to AnansiCapture
+</div>
+
+<div class="toolbar">
+  <!-- Toolbar content will be added here -->
+</div>
+
+<slot /> 
